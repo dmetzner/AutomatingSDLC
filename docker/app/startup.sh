2528854d3574a7d1e9e432d6a433ffaf5e060947
schnider94 <fabian.schnider@student.tugraz.at>
2020-02-11 01:41:07 +0100

    Added more documentation and introduction to the README and added a script to reset permissions and run grunt.

--- docker/app/startup.sh
+++ docker/app/startup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if ./docker/php/wait-for-it.sh db.catroweb.dev:3306; then
+if ./docker/app/wait-for-it.sh db.catroweb.dev:3306; then
     if bin/console doctrine:migrations:migrate ; then
         /usr/sbin/apache2ctl -D FOREGROUND
     fi
