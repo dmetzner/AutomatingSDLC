0dc83cee167394f93cf195d1c8ea0a80e6785690
Max Fazeli <m.fazeli@student.tugraz.at>
2020-03-03 16:12:05 +0100

    Revert "Add sh files from pulling"
    This reverts commit b1600befb4e7d3f10749a03963b5001612af86fa.

--- x_chrome.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-google-chrome-stable --headless --remote-debugging-address=127.0.0.1 --remote-debugging-port=9222 --use-mobile-user-agent
--- x_prestart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ~/Catroweb-Symfony
-git checkout develop
-git pull catroweb develop
-git push
-./x_reset.sh
